1628175cb4d2f7204ca8647ba05fd4eab0539024
Abdallah Alyamni <A.Alyamni@kamel.sa>
2023-06-25 17:09:14 +0300

    - add arabic support using ArPHP

--- src/PDF.php
+++ src/PDF.php
@@ -11,6 +11,7 @@ use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
+use ArPHP\I18N\Arabic;
 
 /**
  * A Laravel wrapper for Dompdf
@@ -86,6 +87,12 @@ class PDF
     public function loadHTML(string $string, ?string $encoding = null): self
     {
         $string = $this->convertEntities($string);
+        $arabic = new Arabic();
+        $p = $arabic->arIdentify($string);
+        for ($i = count($p) - 1; $i >= 0; $i -= 2) {
+            $utf8ar = $arabic->utf8Glyphs(substr($string, $p[$i - 1], $p[$i] - $p[$i - 1]));
+            $string = substr_replace($string, $utf8ar, $p[$i - 1], $p[$i] - $p[$i - 1]);
+        }
         $this->dompdf->loadHtml($string, $encoding);
         $this->rendered = false;
         return $this;
